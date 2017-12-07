7685f24e29c9bdb418f72a26540c177713ef3fcb
MicroKibaco <569015640@qq.com>
2017-12-07 11:42:08 +0800

    添加及查看Task(I)

--- js/base.js
+++ js/base.js
@@ -1,8 +1,56 @@
 ;(function  () {
 	'use strict';
-	alert(1);
-	var a = 1;
-	console.log('a',a);
+
+	var $from_add_task = $('.add-task')
+		,new_task = {}
+		,task_list = {}
+		;
+	  init();
+	$from_add_task.on('submit',function (e) {
+		
+		/**
+		 * 禁用默认行为
+		 */
+		e.preventDefault();
+		/**
+		 * 获取新task的值
+		 */
+		new_task.content = $(this).find('input[name = content]').val();
+		
+		/**
+		 * 如果新task值为空 则直接返回 否则继续执行
+		 */
+		if (!new_task.content) return;
+		
+		/**
+		 * 存入新task
+		 */
+		add_task(new_task);
+		console.log('new_task',new_task);
+		
+		function add_task(new_task) {
+			/**
+			 * 将新的task推入TaskList
+			 */
+			task_list.push(new_task);
+			/**
+			 * 更新localStorage
+			 */
+			store.set('task_list',task_list);
+			
+			console.log('task_list',task_list)
+		}
+		
+		
+		function init() {
+			
+			task_list =  store.get('task_list');
+		
+		}
+	
+	})
+	
+	
 })();
 
 
