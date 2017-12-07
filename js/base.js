db4c7be2e68f8b408d3e3f0e28c0f78b90142137
MicroKibaco <569015640@qq.com>
2017-12-07 15:53:51 +0800

    添加及查看Task(I) 存在Bug 对JQuery语法不太熟悉

--- js/base.js
+++ js/base.js
@@ -1,12 +1,59 @@
-;(function  () {
+;(function () {
 	'use strict';
-
+	
 	var $from_add_task = $('.add-task')
-		,new_task = {}
-		,task_list = {}
-		;
-	  init();
-	$from_add_task.on('submit',function (e) {
+		, new_task = {}
+		, task_list = {}
+	;
+	init();
+
+	
+	function add_task(new_task) {
+		/**
+		 * 将新的task推入TaskList
+		 */
+		task_list.push(new_task);
+		/**
+		 * 更新localStorage
+		 */
+		store.set('task_list', task_list);
+		console.log('task_list', task_list)
+		return true;
+		
+	}
+	
+	function init() {
+		
+		task_list = store.get('task_list') || [];
+		
+	}
+	
+	function render_task_list() {
+		
+		for (var i = 0; i < render_task_tpl; i++) {
+			var $task = render_task_tpl(task_list[i]);
+			
+		}
+		
+	}
+	
+	function render_task_tpl(data) {
+		
+		var list_item_tpl = '<div class="task-item">' +
+		                    '<span class="task-content"><input type="checkbox"></span>' +
+		                    '<span class="task-content">' +
+		                    data.content +
+		                    '</span>' +
+		                    '<span>删除</span>' +
+		                    '<span>详情</span>' +
+		                    '</div>';
+		
+		return $(list_item_tpl);
+		
+	}
+	
+	
+	$from_add_task.on('submit', function (e) {
 		
 		/**
 		 * 禁用默认行为
@@ -15,42 +62,25 @@
 		/**
 		 * 获取新task的值
 		 */
-		new_task.content = $(this).find('input[name = content]').val();
+		new_task.content = $(this).find('input[name=content]').val();
 		
 		/**
 		 * 如果新task值为空 则直接返回 否则继续执行
 		 */
-		if (!new_task.content) return;
-		
+		if (!new_task.content) {
+			return;
+		}
+		console.log('new_task', new_task);
 		/**
 		 * 存入新task
 		 */
-		add_task(new_task);
-		console.log('new_task',new_task);
-		
-		function add_task(new_task) {
-			/**
-			 * 将新的task推入TaskList
-			 */
-			task_list.push(new_task);
-			/**
-			 * 更新localStorage
-			 */
-			store.set('task_list',task_list);
-			
-			console.log('task_list',task_list)
+		if (add_task(new_task)) {
+			render_task_list();
 		}
 		
 		
-		function init() {
-			
-			task_list =  store.get('task_list');
 		
-		}
-	
-	})
-	
-	
+	});
 })();
 
 
